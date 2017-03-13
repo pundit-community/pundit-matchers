3e1e750c29ee275c0a8a89cb5f21690127b00973
Chris Alley <chris@chrisalley.info>
2017-03-13 20:57:31 +1100

    Remove ./lib/ from require statements

--- lib/pundit/matchers.rb
+++ lib/pundit/matchers.rb
@@ -1,15 +1,15 @@
 require 'rspec/core'
 
-require './lib/pundit/matchers/forbid_action'
-require './lib/pundit/matchers/forbid_actions'
-require './lib/pundit/matchers/forbid_edit_and_update_actions'
-require './lib/pundit/matchers/forbid_mass_assignment_of'
-require './lib/pundit/matchers/forbid_new_and_create_actions'
-require './lib/pundit/matchers/permit_action'
-require './lib/pundit/matchers/permit_actions'
-require './lib/pundit/matchers/permit_edit_and_update_actions'
-require './lib/pundit/matchers/permit_mass_assignment_of'
-require './lib/pundit/matchers/permit_new_and_create_actions'
+require 'pundit/matchers/forbid_action'
+require 'pundit/matchers/forbid_actions'
+require 'pundit/matchers/forbid_edit_and_update_actions'
+require 'pundit/matchers/forbid_mass_assignment_of'
+require 'pundit/matchers/forbid_new_and_create_actions'
+require 'pundit/matchers/permit_action'
+require 'pundit/matchers/permit_actions'
+require 'pundit/matchers/permit_edit_and_update_actions'
+require 'pundit/matchers/permit_mass_assignment_of'
+require 'pundit/matchers/permit_new_and_create_actions'
 
 if defined?(Pundit)
   RSpec.configure do |config|
