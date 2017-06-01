16463a658a3a6d66773dedbb3538468b106a305b
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-01 12:29:43 +0200

    Add support for action arguments

--- spec/matchers/forbid_action_spec.rb
+++ spec/matchers/forbid_action_spec.rb
@@ -26,4 +26,60 @@ describe 'forbid_action matcher' do
     subject { ForbidActionTestPolicy2.new }
     it { is_expected.to forbid_action(:test) }
   end
+
+  context 'test_arg? is forbidden' do
+    before do
+      class ForbidActionTestPolicy3
+        def test_arg?(argument)
+          raise unless argument == 'argument'
+          true
+        end
+      end
+    end
+
+    subject { ForbidActionTestPolicy3.new }
+    it { is_expected.not_to forbid_action(:test_arg, 'argument') }
+  end
+
+  context 'test_arg? is forbidden' do
+    before do
+      class ForbidActionTestPolicy4
+        def test_arg?(argument)
+          raise unless argument == 'argument'
+          false
+        end
+      end
+    end
+
+    subject { ForbidActionTestPolicy4.new }
+    it { is_expected.to forbid_action(:test_arg, 'argument') }
+  end
+
+  context 'test_multiple_arg? is forbidden' do
+    before do
+      class ForbidActionTestPolicy5
+        def test_multiple_arg?(one, two, three)
+          raise unless one == 'one' && two == 'two' && three == 'three'
+          true
+        end
+      end
+    end
+
+    subject { ForbidActionTestPolicy5.new }
+    it { is_expected.not_to forbid_action(:test_multiple_arg, 'one', 'two', 'three') }
+  end
+
+  context 'test_multiple_arg? is forbidden' do
+    before do
+      class ForbidActionTestPolicy6
+        def test_multiple_arg?(one, two, three)
+          raise unless one == 'one' && two == 'two' && three == 'three'
+          false
+        end
+      end
+    end
+
+    subject { ForbidActionTestPolicy6.new }
+    it { is_expected.to forbid_action(:test_multiple_arg, 'one', 'two', 'three') }
+  end
 end
