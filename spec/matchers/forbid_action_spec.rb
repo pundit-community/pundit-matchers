b96860ff0eb044bb4e36984be0a9a0cb487c68ab
Chris Alley <chris@chrisalley.info>
2017-06-03 23:45:53 +1000

    Refactor singular action specs into clearer contexts

--- spec/matchers/forbid_action_spec.rb
+++ spec/matchers/forbid_action_spec.rb
@@ -1,85 +1,91 @@
 require 'rspec/core'
 
 describe 'forbid_action matcher' do
-  context 'test? is permitted' do
-    before do
-      class ForbidActionTestPolicy1
-        def test?
-          true
+  context 'no optional arguments are specified' do
+    context 'test? is permitted' do
+      before do
+        class ForbidActionTestPolicy1
+          def test?
+            true
+          end
         end
       end
+
+      subject { ForbidActionTestPolicy1.new }
+      it { is_expected.not_to forbid_action(:test) }
     end
 
-    subject { ForbidActionTestPolicy1.new }
-    it { is_expected.not_to forbid_action(:test) }
+    context 'test? is forbidden' do
+      before do
+        class ForbidActionTestPolicy2
+          def test?
+            false
+          end
+        end
+      end
+
+      subject { ForbidActionTestPolicy2.new }
+      it { is_expected.to forbid_action(:test) }
+    end
   end
 
-  context 'test? is forbidden' do
-    before do
-      class ForbidActionTestPolicy2
-        def test?
-          false
+  context 'one optional argument is specified' do
+    context 'test? with optional argument is permitted' do
+      before do
+        class ForbidActionTestPolicy3
+          def test?(argument)
+            raise unless argument == 'argument'
+            true
+          end
         end
       end
+
+      subject { ForbidActionTestPolicy3.new }
+      it { is_expected.not_to forbid_action(:test, 'argument') }
     end
 
-    subject { ForbidActionTestPolicy2.new }
-    it { is_expected.to forbid_action(:test) }
+    context 'test? with optional argument is forbidden' do
+      before do
+        class ForbidActionTestPolicy4
+          def test?(argument)
+            raise unless argument == 'argument'
+            false
+          end
+        end
+      end
+
+      subject { ForbidActionTestPolicy4.new }
+      it { is_expected.to forbid_action(:test, 'argument') }
+    end
   end
 
-  context 'test_arg? is forbidden' do
-    before do
-      class ForbidActionTestPolicy3
-        def test_arg?(argument)
-          raise unless argument == 'argument'
-          true
+  context 'more than one argument is specified' do
+    context 'test? with optional arguments is permitted' do
+      before do
+        class ForbidActionTestPolicy5
+          def test?(one, two, three)
+            raise unless one == 'one' && two == 'two' && three == 'three'
+            true
+          end
         end
       end
+
+      subject { ForbidActionTestPolicy5.new }
+      it { is_expected.not_to forbid_action(:test, 'one', 'two', 'three') }
     end
 
-    subject { ForbidActionTestPolicy3.new }
-    it { is_expected.not_to forbid_action(:test_arg, 'argument') }
-  end
-
-  context 'test_arg? is forbidden' do
-    before do
-      class ForbidActionTestPolicy4
-        def test_arg?(argument)
-          raise unless argument == 'argument'
-          false
+    context 'test? with optional arguments is forbidden' do
+      before do
+        class ForbidActionTestPolicy6
+          def test?(one, two, three)
+            raise unless one == 'one' && two == 'two' && three == 'three'
+            false
+          end
         end
       end
+
+      subject { ForbidActionTestPolicy6.new }
+      it { is_expected.to forbid_action(:test, 'one', 'two', 'three') }
     end
-
-    subject { ForbidActionTestPolicy4.new }
-    it { is_expected.to forbid_action(:test_arg, 'argument') }
-  end
-
-  context 'test_multiple_arg? is forbidden' do
-    before do
-      class ForbidActionTestPolicy5
-        def test_multiple_arg?(one, two, three)
-          raise unless one == 'one' && two == 'two' && three == 'three'
-          true
-        end
-      end
-    end
-
-    subject { ForbidActionTestPolicy5.new }
-    it { is_expected.not_to forbid_action(:test_multiple_arg, 'one', 'two', 'three') }
-  end
-
-  context 'test_multiple_arg? is forbidden' do
-    before do
-      class ForbidActionTestPolicy6
-        def test_multiple_arg?(one, two, three)
-          raise unless one == 'one' && two == 'two' && three == 'three'
-          false
-        end
-      end
-    end
-
-    subject { ForbidActionTestPolicy6.new }
-    it { is_expected.to forbid_action(:test_multiple_arg, 'one', 'two', 'three') }
   end
 end
--- spec/matchers/permit_action_spec.rb
+++ spec/matchers/permit_action_spec.rb
@@ -1,85 +1,91 @@
 require 'rspec/core'
 
 describe 'permit_action matcher' do
-  context 'test? is permitted' do
-    before do
-      class PermitActionTestPolicy1
-        def test?
-          true
+  context 'no arguments are specified' do
+    context 'test? is permitted' do
+      before do
+        class PermitActionTestPolicy1
+          def test?
+            true
+          end
         end
       end
+
+      subject { PermitActionTestPolicy1.new }
+      it { is_expected.to permit_action(:test) }
     end
 
-    subject { PermitActionTestPolicy1.new }
-    it { is_expected.to permit_action(:test) }
+    context 'test? is forbidden' do
+      before do
+        class PermitActionTestPolicy2
+          def test?
+            false
+          end
+        end
+      end
+
+      subject { PermitActionTestPolicy2.new }
+      it { is_expected.not_to permit_action(:test) }
+    end
   end
 
-  context 'test? is forbidden' do
-    before do
-      class PermitActionTestPolicy2
-        def test?
-          false
+  context 'one argument is specified' do
+    context 'test? with optional argument is permitted' do
+      before do
+        class PermitActionTestPolicy3
+          def test?(argument)
+            raise unless argument == 'argument'
+            true
+          end
         end
       end
+
+      subject { PermitActionTestPolicy3.new }
+      it { is_expected.to permit_action(:test, 'argument') }
     end
 
-    subject { PermitActionTestPolicy2.new }
-    it { is_expected.not_to permit_action(:test) }
+    context 'test? with optional argument is forbidden' do
+      before do
+        class PermitActionTestPolicy4
+          def test?(argument)
+            raise unless argument == 'argument'
+            false
+          end
+        end
+      end
+
+      subject { PermitActionTestPolicy4.new }
+      it { is_expected.not_to permit_action(:test, 'argument') }
+    end
   end
 
-  context 'test_arg? is permitted' do
-    before do
-      class PermitActionTestPolicy3
-        def test_arg?(argument)
-          raise unless argument == 'argument'
-          true
+  context 'more than one argument is specified' do
+    context 'test? with optional arguments is permitted' do
+      before do
+        class PermitActionTestPolicy5
+          def test?(one, two, three)
+            raise unless one == 'one' && two == 'two' && three == 'three'
+            true
+          end
         end
       end
+
+      subject { PermitActionTestPolicy5.new }
+      it { is_expected.to permit_action(:test, 'one', 'two', 'three') }
     end
 
-    subject { PermitActionTestPolicy3.new }
-    it { is_expected.to permit_action(:test_arg, 'argument') }
-  end
-
-  context 'test_arg? is forbidden' do
-    before do
-      class PermitActionTestPolicy4
-        def test_arg?(argument)
-          raise unless argument == 'argument'
-          false
+    context 'test? with optional arguments is forbidden' do
+      before do
+        class PermitActionTestPolicy6
+          def test?(one, two, three)
+            raise unless one == 'one' && two == 'two' && three == 'three'
+            false
+          end
         end
       end
+
+      subject { PermitActionTestPolicy6.new }
+      it { is_expected.not_to permit_action(:test, 'one', 'two', 'three') }
     end
-
-    subject { PermitActionTestPolicy4.new }
-    it { is_expected.not_to permit_action(:test_arg, 'argument') }
-  end
-
-  context 'test_multiple_arg? is permitted' do
-    before do
-      class PermitActionTestPolicy5
-        def test_multiple_arg?(one, two, three)
-          raise unless one == 'one' && two == 'two' && three == 'three'
-          true
-        end
-      end
-    end
-
-    subject { PermitActionTestPolicy5.new }
-    it { is_expected.to permit_action(:test_multiple_arg, 'one', 'two', 'three') }
-  end
-
-  context 'test_multiple_arg? is forbidden' do
-    before do
-      class PermitActionTestPolicy6
-        def test_multiple_arg?(one, two, three)
-          raise unless one == 'one' && two == 'two' && three == 'three'
-          false
-        end
-      end
-    end
-
-    subject { PermitActionTestPolicy6.new }
-    it { is_expected.not_to permit_action(:test_multiple_arg, 'one', 'two', 'three') }
   end
 end
