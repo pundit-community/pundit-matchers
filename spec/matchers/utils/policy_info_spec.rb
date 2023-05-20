91e8b9ce0a7e6cca5eb96c7bf9d8c50f6f3b2b2e
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-05-20 11:34:44 +0200

    Improve Policy Info
    - Add `to_s`
    - Add `user`
    - Memoize `forbidden_actions`

--- spec/matchers/utils/policy_info_spec.rb
+++ spec/matchers/utils/policy_info_spec.rb
@@ -3,12 +3,35 @@
 RSpec.describe Pundit::Matchers::Utils::PolicyInfo do
   subject(:policy_info) { described_class.new(policy) }
 
-  let(:policy) { policy_factory(update?: false, create?: true, new?: true) }
+  let(:policy) do
+    policy_factory(update?: false, create?: true, new?: true,
+                   user: 'Test User', custom_user: 'Custom Test User')
+  end
+
+  describe '#to_s' do
+    subject { policy_info.to_s }
+
+    it { is_expected.to eq 'TestPolicy' }
+  end
+
+  describe '#user' do
+    subject { policy_info.user }
+
+    it { is_expected.to eq 'Test User' }
+
+    context 'with a custom user configuration' do
+      before do
+        allow(Pundit::Matchers.configuration).to receive(:user_alias).and_return(:custom_user)
+      end
+
+      it { is_expected.to eq 'Custom Test User' }
+    end
+  end
 
   describe '#actions' do
     subject(:actions) { policy_info.actions }
 
-    it 'returns correct actions' do
+    it 'returns all actions alphabetically sorted' do
       expect(actions).to eq(%i[create new update])
     end
   end
@@ -16,7 +39,7 @@ RSpec.describe Pundit::Matchers::Utils::PolicyInfo do
   describe '#permitted_actions' do
     subject(:permitted_actions) { policy_info.permitted_actions }
 
-    it 'returns actions with "true" result only' do
+    it 'returns permitted actions' do
       expect(permitted_actions).to eq(%i[create new])
     end
   end
@@ -24,7 +47,7 @@ RSpec.describe Pundit::Matchers::Utils::PolicyInfo do
   describe '#forbidden_actions' do
     subject(:forbidden_actions) { policy_info.forbidden_actions }
 
-    it 'returns actions with "false" result only' do
+    it 'returns forbidden actions' do
       expect(forbidden_actions).to contain_exactly(:update)
     end
   end
